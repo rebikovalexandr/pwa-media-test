37c1573e8afcf8eb85775866291ac9fd955cf319
rebikovalexandr <alexandrreb22@gmail.com>
2024-04-02 13:36:45 +0200

    add workflow

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.806rt28rck8"
+    "revision": "0.u23d7317sh8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({command}) => ({
+  base: command !== 'serve' ? '/pwa-media-test/' : '/',
   plugins: [react(), VitePWA({
     registerType: 'autoUpdate',
     // strategies: 'injectManifest',
@@ -31,4 +32,4 @@ export default defineConfig({
       ]
     }
   })],
-})
+}))
